e9cd8f04668bd2ce660485aeb87e864868a3a658
saisandeep.yalamanchili <saisandeep.yalamanchili@cgi.com>
2019-09-24 18:36:12 +0530

    initial commit

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,22 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Movie {
+    @Autowired
+    private Actor actor;
+
+    @Autowired
+    public void setValues() {
+
+        actor.setAge(21);
+        actor.setGender("male");
+        actor.setName("Sandeep");
+    }
+    public void displayActorDetails()
+    {
+        System.out.println(this.actor.getName()+ " "+ this.actor.getGender()+ " "+ this.actor.getAge());
+    }
+}
