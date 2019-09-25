00b08eb14d81f4b056780d81c61912d71b57f07b
saisandeep.yalamanchili <saisandeep.yalamanchili@cgi.com>
2019-09-25 12:47:14 +0530

    beanlifecycle commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -3,16 +3,18 @@ package com.stackroute;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 
 public class Main{
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 //        Movie movie= context.getBean(Movie.class);
 //        movie.displayActorDetails();
         Movie movie1 =context.getBean(Movie.class);
         movie1.displayActorDetails();
+        context.registerShutdownHook();
 
 //        Movie movie1 = context.getBean("movie1",Movie.class);
 //        movie1.displayActorDetails();
--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -1,4 +1,19 @@
 package com.stackroute.demo;
 
-public class BeanLifecycleDemoBean {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanLifecycleDemoBean  implements DisposableBean, InitializingBean {
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("just before destroying");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("just after propertiesSet");
+
+    }
 }
